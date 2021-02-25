fb2fc1f531fdcd617b5615da72b08f4185832492
Luana Campos <luana.cam20@gmail.com>
2021-02-25 18:17:52 -0300

    Day 4

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
+import Head from 'next/head';
 
 import { CompletedChallenges } from "../components/CompleteChallanges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
+import { ChallengeBox } from "../components/ChallengeBox";
 
-import Head from 'next/head';
 
 import styles from '../styles/pages/Home.module.css'
+
 
 export default function Home() {
   return (
@@ -22,7 +24,7 @@ export default function Home() {
         <Countdown />
       </div>
       <div>
-
+        <ChallengeBox />
       </div>
     </section>
 </div>
